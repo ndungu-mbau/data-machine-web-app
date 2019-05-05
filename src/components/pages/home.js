b877f9b7d03f378f92f0132e5e412e66f789dc5a
gitomehbranson <sirbranson67@gmail.com>
2019-05-05 10:56:23 +0300

    added sidebar

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
-
+import Sidebar from "../layouts/sidebar"
 
 
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                <h1>home</h1>
+            <div className="k-grid k-grid--hor k-grid--root">
+                <div className="k-grid__item k-grid__item--fluid k-grid k-grid--ver k-page">
+                    <button id="k_aside_close_btn" class="k-aside-close"><i class="la la-close"></i></button>
+                    <Sidebar></Sidebar>
+                </div>
             </div>
         );
     }
