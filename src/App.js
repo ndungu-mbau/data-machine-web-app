f5c10b19d8b2e0df8d0d6a68e75597733a0a8f9c
Nelson Mbau <sirmbau@gmail.com>
2019-05-05 11:13:23 +0300

    Added prettier config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import HomeComponent from './components/pages/home'
-import AboutComponent from './components/pages/about'
-import ContactUsComponent from './components/pages/contact'
+import HomeComponent from './components/pages/home';
+import AboutComponent from './components/pages/about';
+import ContactUsComponent from './components/pages/contact';
 
 class App extends Component {
   render() {
     return (
-
       <Router>
         <div>
-          <Route exact path='/' component={HomeComponent}></Route>
-          <Route exact path='/about' component={AboutComponent}></Route>
-          <Route exact path='/contacts' component={ContactUsComponent}></Route>
+          <Route exact path="/" component={HomeComponent} />
+          <Route exact path="/about" component={AboutComponent} />
+          <Route exact path="/contacts" component={ContactUsComponent} />
         </div>
       </Router>
-
     );
   }
 }
