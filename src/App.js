8ad96b4e2d3e2d45e4f5b331507c3be20b7dab6c
davidmungai <mungahdaudi@gmail.com>
2019-05-05 12:21:25 +0300

    added login page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomeComponent from './components/pages/home';
 import AboutComponent from './components/pages/about';
 import ContactUsComponent from './components/pages/contact';
+import LoginComponent from './components/pages/login';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <Route exact path="/login" component={LoginComponent} />
           <Route exact path="/" component={HomeComponent} />
           <Route exact path="/about" component={AboutComponent} />
           <Route exact path="/contacts" component={ContactUsComponent} />
