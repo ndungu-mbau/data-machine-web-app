98bca33b9e75d22e9a560ea6e9c7ed806903bc5e
GitomehBranson <sirbranson67@gmail.com>
2019-05-09 10:23:10 +0300

    fix login issues

--- src/app/components/pages/login.js
+++ src/app/components/pages/login.js
@@ -9,6 +9,7 @@ export default class Login extends Component {
     this.state = {
       password: '',
       phone: '',
+      showPass: false,
     };
   }
 
@@ -18,137 +19,155 @@ export default class Login extends Component {
   passChange(event) {
     this.setState({ password: event.target.value });
   }
-  async authUser(event) {
-    event.preventDefault();
-    try {
-      var response = await requestBackend({ ...this.state }, '/auth/login');
-      console.log(response.data);
-      window.localStorage.setItem(
-        'user',
-        JSON.stringify(
-          Object.assign({}, response.data, {
-            _id: undefined,
-            token: undefined,
-          }),
-        ),
-      );
-      window.localStorage.setItem('token', response.data.token);
-      return this.props.history.push('/');
-    } catch (err) {
-      if (err.response.data) toast.error(err.response.data.message);
-    }
+  componentDidMount() {
+    const forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    Array.prototype.filter.call(forms, form => {
+      form.addEventListener('submit', async event => {
+        event.preventDefault();
+        if (form.checkValidity() === false) {
+          event.stopPropagation();
+          return form.classList.add('was-validated');
+        }
+
+        const { phone, password } = this.state;
+
+        try {
+          var response = await requestBackend({ phone, password }, '/auth/login');
+          console.log(response.data);
+          window.localStorage.setItem(
+            'user',
+            JSON.stringify(
+              Object.assign({}, response.data, {
+                _id: undefined,
+                token: undefined,
+              }),
+            ),
+          );
+          window.localStorage.setItem('token', response.data.token);
+          return this.props.history.push('/');
+        } catch (err) {
+          if (err.response.data) toast.error(err.response.data.message);
+        }
+      });
+    });
   }
   render() {
-    return (
-      <>
-        <Helmet>
-          <link
-            rel="stylesheet"
-            href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-            integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-            crossorigin="anonymous"
-          />
-          <link
-            rel="stylesheet"
-            href="https://preview.webpixels.io/purpose-website-ui-kit-v2.0.0/assets/css/purpose.css"
-            id="stylesheet"
-          />
-        </Helmet>
-        <div className="g-sidenav-show g-sidenav-pinned main-content container h-100vh d-flex align-items-center">
-          <div className="col">
-            <div className="row justify-content-center">
-              <div className="col-md-6 col-lg-5 col-xl-4">
-                <div>
-                  <div className="mb-5 text-center">
-                    <p>Login</p>
-                    <img src="./assets/media/logos/all-black.svg" alt="" />
-                  </div>
-                  <span className="clearfix" />
-                  {this.state.error ? (
-                    <div role="alert" className="alert alert-danger">
-                      <div className="alert-icon">
-                        <i className="flaticon-questions-circular-button" />
-                      </div>
-                      <div className="alert-text">{this.state.error}</div>
+    return [
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://preview.webpixels.io/purpose-website-ui-kit-v2.0.0/assets/css/purpose.css"
+          id="stylesheet"
+        />
+      </Helmet>,
+      <div className="g-sidenav-show g-sidenav-pinned main-content container h-100vh d-flex align-items-center">
+        <div className="col">
+          <div className="row justify-content-center">
+            <div className="col-md-6 col-lg-5 col-xl-4">
+              <div>
+                <div className="mb-5 text-center">
+                  <p>Login</p>
+                  <img src="./assets/media/logos/all-black.svg" alt="" />
+                </div>
+                <span className="clearfix" />
+                {this.state.error ? (
+                  <div role="alert" className="alert alert-danger">
+                    <div className="alert-icon">
+                      <i className="flaticon-questions-circular-button" />
                     </div>
-                  ) : (
-                    ''
-                  )}
-                  <form noValidate className="needs-validation">
-                    <div className="form-group">
-                      <label htmlFor="input-mail" className="form-control-label">
-                        phone
-                      </label>
+                    <div className="alert-text">{this.state.error}</div>
+                  </div>
+                ) : (
+                  ''
+                )}
+                <form noValidate className="needs-validation">
+                  <div className="form-group">
+                    <label htmlFor="input-mail" className="form-control-label">
+                      Your Phone Number
+                    </label>
+                    <div className="input-group input-group-merge">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text">
+                          <i className="fas fa-user" />
+                        </span>
+                      </div>
+                      <input
+                        data-error="Please enter a valid phone number"
+                        required
+                        type="text"
+                        id="input-mail"
+                        placeholder="ie. 0712 345 789"
+                        className="form-control"
+                        onChange={e => {
+                          this.setState({ phone: e.target.value });
+                        }}
+                        value={this.state.phone}
+                      />
+                    </div>
+                    <div className="invalid-feedback">Please enter a valid email</div>
+                    <div className="valid-feedback">looks ok</div>
+                  </div>
+                  <div className="form-group mb-4">
+                    <div className="d-flex align-items-center justify-content-between">
+                      <div>
+                        <label className="form-control-label">Your Password</label>
+                      </div>
+                    </div>
+                    <div className="input-group input-group-merge">
                       <div className="input-group input-group-merge">
                         <div className="input-group-prepend">
                           <span className="input-group-text">
-                            <i className="fas fa-user" />
+                            <i className="fas fa-key" />
                           </span>
                         </div>
                         <input
-                          data-error="Please enter a valid email"
+                          data-error="Please enter a password"
                           required
-                          type="number"
-                          id="input-mail"
-                          placeholder="ie. 131 2323 232"
+                          type={this.state.showPass === true ? 'text' : 'password'}
+                          id="input-password"
+                          placeholder="Password"
                           className="form-control"
-                          onChange={this.handleChange.bind(this)}
-                          value={this.state.phone}
+                          onChange={e => {
+                            this.setState({ password: e.target.value });
+                          }}
+                          value={this.state.password}
                         />
+                        <div className="input-group-append" style={{ cursor: 'pointer' }}>
+                          <span
+                            onClick={() => {
+                              this.setState({ showPass: true });
+                              setTimeout(() => {
+                                this.setState({ showPass: false });
+                              }, 2000);
+                            }}
+                            className="input-group-text"
+                          >
+                            <i className="fas fa-eye" />
+                          </span>
+                        </div>
                       </div>
-                      <div className="invalid-feedback">Please enter a valid email</div>
+                      <div className="invalid-feedback">Please enter a password</div>
                       <div className="valid-feedback">looks ok</div>
                     </div>
-                    <div className="form-group mb-4">
-                      <div className="d-flex align-items-center justify-content-between">
-                        <div>
-                          <label className="form-control-label">Password</label>
-                        </div>
-                      </div>
-                      <div className="input-group input-group-merge">
-                        <div className="input-group input-group-merge">
-                          <div className="input-group-prepend">
-                            <span className="input-group-text">
-                              <i className="fas fa-key" />
-                            </span>
-                          </div>
-                          <input
-                            data-error="Please enter a password"
-                            required
-                            type="password"
-                            id="input-password"
-                            placeholder="Password"
-                            className="form-control"
-                            onChange={this.passChange.bind(this)}
-                            value={this.state.password}
-                          />
-                          <div className="input-group-append" style={{ cursor: 'pointer' }}>
-                            <span className="input-group-text">
-                              <i className="fas fa-eye" />
-                            </span>
-                          </div>
-                        </div>
-                        <div className="invalid-feedback">Please enter a password</div>
-                        <div className="valid-feedback">looks ok</div>
-                      </div>
-                    </div>
-                    <div className="mt-4">
-                      <button
-                        type="submit"
-                        onClick={e => this.authUser(e)}
-                        className="btn btn-block btn-primary"
-                      >
-                        {' '}
-                        Sign in
-                      </button>
-                    </div>
-                  </form>
-                </div>
+                  </div>
+                  <div className="mt-4">
+                    <button type="submit" className="btn btn-block btn-primary">
+                      Sign in
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
-      </>
-    );
+      </div>,
+    ];
   }
 }
