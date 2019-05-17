de5395e755a6539cd75b997dc6cb367bc2b10b80
GitomehBranson <sirbranson67@gmail.com>
2019-05-17 10:09:54 +0300

    fix: support for multichoice questions as parents

--- src/app/components/layouts/decider.js
+++ src/app/components/layouts/decider.js
@@ -149,7 +149,15 @@ export default class Decider extends Component {
           {options.map(option => {
             return (
               <label key={option.label + option.value} className="k-checkbox">
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  onChange={e => {
+                    this.setAnswer({
+                      tag: `${tag}_option_${option.label.replace(/\s/g, '_')}`,
+                      value: option.value,
+                    });
+                  }}
+                />
                 {option.label}
                 <span />
               </label>
