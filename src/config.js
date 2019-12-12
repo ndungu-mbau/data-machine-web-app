8241626e78f18f277643331302632c76328f808c
BransonGitomeh <sirbranson67@gmail.com>
2019-12-12 21:07:12 +0300

    chore: add appropriate urls's

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 export default {
-  production: {
-    apiUrl: 'https://graph.braiven.io',
-    appUrl: 'https://braiven.io',
-    log_level: 'silent',
-  },
-  staging: {
-    apiUrl: 'https://staging.graph.braiven.io',
-    appUrl: 'https://staging.braiven.io',
-    log_level: 'silent',
-  },
-  development: {
-    apiUrl: 'http://localhost:4000',
-    appUrl: 'http://localhost:3002',
-    log_level: undefined,
-  },
+    production: {
+        apiUrl: 'https://databank-api-server.herokuapp.com',
+        appUrl: 'https://databank-web.herokuapp.com/',
+        log_level: 'silent',
+    },
+    staging: {
+        apiUrl: 'https://staging.graph.braiven.io',
+        appUrl: 'https://staging.braiven.io',
+        log_level: 'silent',
+    },
+    development: {
+        apiUrl: 'http://localhost:4000',
+        appUrl: 'http://localhost:3002',
+        log_level: undefined,
+    },
 };
